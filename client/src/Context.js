f4ea499f2460677947301dba7be872f10d865fdc
valwebd <77312053+valwebd@users.noreply.github.com>
2021-09-21 10:51:29 -0700

    heroku server

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const Context = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://poom-server.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
